1144e0081bb280b778c013714d5cae2dfb075ca4
Jonsen Yang <im16hot@gmail.com>
2020-08-14 16:44:17 +0800

    change init data dir

--- opencc.go
+++ opencc.go
@@ -1,15 +1,14 @@
 package opencc
 
 import (
-	"flag"
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"encoding/json"
-	"strings"
-	"path/filepath"
 	"os"
-	"bufio"
+	"path/filepath"
+	"strings"
 )
 
 var punctuations []string = []string{
@@ -17,12 +16,16 @@ var punctuations []string = []string{
 	"，", "。", "、", "；", "：", "？", "！", "…", "“", "”", "「",
 	"」", "—", "－", "（", "）", "《", "》", "．", "／", "＼"}
 
-func init() {
-	flag.StringVar(&dataDir, "data", "", "config data direct.")
-}
+// func init() {
+// 	flag.StringVar(&dataDir, "data", "", "config data direct.")
+// }
 
 type OpenCC struct {
 	conf *Config
+}
+
+func InitData(dir string) {
+	dataDir = dir
 }
 
 // Supported conversions: s2t, t2s, s2tw, tw2s, s2hk, hk2s, s2twp, tw2sp, t2tw, t2hk
@@ -45,14 +48,15 @@ func NewOpenCC(conversions string) (*OpenCC, error) {
 		return nil, err
 	}
 	//
-	return &OpenCC{conf:conf}, nil
+	return &OpenCC{conf: conf}, nil
 }
+
 //
 func (oc *OpenCC) ConvertFile(in io.Reader, out io.Writer) error {
 	inReader := bufio.NewReader(in)
 	for {
 		lineText, readErr := inReader.ReadString('\n')
-		if readErr != nil && readErr != io.EOF{
+		if readErr != nil && readErr != io.EOF {
 			return readErr
 		}
 		nLineText, err := oc.splitText(lineText)
@@ -63,22 +67,24 @@ func (oc *OpenCC) ConvertFile(in io.Reader, out io.Writer) error {
 		if err != nil {
 			return err
 		}
-		if readErr == io.EOF{
+		if readErr == io.EOF {
 			break
 		}
 	}
 	return nil
 }
+
 //
 func (oc *OpenCC) ConvertText(text string) (string, error) {
 	return oc.splitText(text)
 }
+
 //
-func (oc *OpenCC) splitText(text string)(string, error){
-	tmp := make([]string,0,len(text))
+func (oc *OpenCC) splitText(text string) (string, error) {
+	tmp := make([]string, 0, len(text))
 	var newText string
-	for i,c := range strings.Split(text,""){
-		if i > 0 &&  isPunctuations(c){
+	for i, c := range strings.Split(text, "") {
+		if i > 0 && isPunctuations(c) {
 			if len(tmp) > 0 {
 				tx, err := oc.convertString(strings.Join(tmp, ""))
 				if err != nil {
@@ -86,27 +92,28 @@ func (oc *OpenCC) splitText(text string)(string, error){
 				}
 				newText = newText + tx + c
 				tmp = tmp[:0]
-			}else{
+			} else {
 				newText = newText + c
 			}
 			continue
 		}
-		tmp = append(tmp,c)
+		tmp = append(tmp, c)
 	}
-	if len(tmp) > 0{
-		tx ,err := oc.convertString(strings.Join(tmp,""))
-		if err != nil{
-			return text,err
+	if len(tmp) > 0 {
+		tx, err := oc.convertString(strings.Join(tmp, ""))
+		if err != nil {
+			return text, err
 		}
 		newText = newText + tx
 	}
-	return newText,nil
+	return newText, nil
 }
+
 //
 func (oc *OpenCC) convertString(text string) (string, error) {
 	var err error
-	if oc.conf == nil{
-		return text,fmt.Errorf("no config")
+	if oc.conf == nil {
+		return text, fmt.Errorf("no config")
 	}
 	text, err = oc.conf.convertText(text)
 	if err != nil {
@@ -114,14 +121,15 @@ func (oc *OpenCC) convertString(text string) (string, error) {
 	}
 	return text, nil
 }
+
 //是否标点符号
-func isPunctuations(character string) bool{
-	if len([]byte(character)) <= 1{
+func isPunctuations(character string) bool {
+	if len([]byte(character)) <= 1 {
 		return true
 	}
 	//
-	for _,c := range punctuations{
-		if c == character{
+	for _, c := range punctuations {
+		if c == character {
 			return true
 		}
 	}
